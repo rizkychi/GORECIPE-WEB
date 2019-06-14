ef91bda9b129209adb882d347330bac892977071
Rizkychi <rizkynhae@gmail.com>
2019-06-14 16:20:15 +0700

    tambah flexbox, halaman akun

--- js/gocip.js
+++ js/gocip.js
@@ -5,21 +5,22 @@
 
 $(document).ready(function(){
     // ---------- Navbar ---------- //
+    var navtop = $(".bot-header").position().top;
     $(window).on("scroll",function(){
-        if ($(window).scrollTop()){
-            $(".top-header").hide();
+        if ($(window).scrollTop() > navtop){
+            
             $(".bot-header").addClass("scrollme");
-            $(".left-menu img").show(250);
+            $(".left-menu img").animate({width:"show"},350);
         } else {
-            $(".top-header").show();
+            
             $(".bot-header").removeClass("scrollme");
-            $(".left-menu img").hide();
+            $(".left-menu img").animate({width:"hide"},350);
         }
     });
 
     // show/hide search input
     $(".submit").click(function(){
-        $("#frmsearch").fadeToggle(250);
+        $("#frmsearch").animate({width:"toggle"},350);
     });
     
     // adding class 'active' to navbar based on current page
---
+++ page/akun.php
@@ -0,0 +1,20 @@
+<div class="container">
+    <div class="flex-grid">
+        <div class="col">
+            <div class="sidebar">
+                <ul>
+                    <li><a href="#">Informasi Umum</a></li>
+                    <li><a href="#">Ubah Sandi</a></li>
+                    <li><a href="#">Akun</a></li>
+                    <li><a href="#">Favorit</a></li>
+                    <li><a href="#">Bantuan</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col" style="flex-grow:3;">
+            <div class="profile">
+                asa
+            </div>
+        </div>
+    </div>
+</div>
