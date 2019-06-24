ad11c7e6d9a48c21abedaadcdacb1c91d41e731e
Muhammad Nur Alfian <muhammad.1220@students.amikom.ac.id>
2019-06-25 06:36:59 +0700

    geser geser

--- page/home.php
+++ page/home.php
@@ -1,143 +1,175 @@
-    <div class="container">
+<div class="container">
 
-        <ul class="slides">
-            <input type="radio" name="rdo-btn" id="img-1" checked>
-            <li class="slide-container">
-                <div class="slide">
-                    <img alt="banner 1" src="img/img_mountains_wide.jpg">
-                </div>
-            </li>
+    <ul class="slides">
+        <input type="radio" name="rdo-btn" id="img-1" checked>
 
-            <input type="radio" name="rdo-btn" id="img-2">
-            <li class="slide-container">
-                <div class="slide">
-                    <img alt="banner 2" src="img/img_nature_wide.jpg">
-                </div>
-            </li>
+        <li class="slide-container">
 
-            <input type="radio" name="rdo-btn" id="img-3">
-            <li class="slide-container">
-                <div class="slide">
-                    <img alt="banner 3" src="img/img_snow_wide.jpg">
-                </div>
-            </li>
-
-            <input type="radio" name="rdo-btn" id="img-4">
-            <li class="slide-container">
-                <div class="slide">
-                    <img alt="banner 4" src="img/img_nature_spring_forest.jpg">
-                </div>
-            </li>
-
-            <li class="dots">
-                <label for="img-1" id="img-dot-1"></label>
-                <label for="img-2" id="img-dot-2"></label>
-                <label for="img-3" id="img-dot-3"></label>
-                <label for="img-4" id="img-dot-4"></label>
-            </li>
-
-        </ul>
-
-        <div class="home-container">
-
-            <div class="info">
-                <div class="info-item animated fadeInLeft"><img src="img/home-info.png" alt="creative-cooking"></div>
-                <div class="info-item animated fadeInRight">
-                    <h1>Berani Untuk Masak Sendiri Mulai Dari Sekarang</h1>
-                    <br>
-                    <ul>
-                        <li>Lebih Higienis dan Tak Perlu Diragukan</li>
-                        <li>Meningkatkan Keahlian Memasak</li>
-                        <li>Lebih Sehat</li>
-                        <li>Salurkan Kreativitas Ke Masakan</li>
-                    </ul>
-                </div>
+            <div class="slide">
+                <img alt="banner 1" src="img/img_mountains_wide.jpg">
             </div>
 
-            <div class="category">
-
-                <div class="cat-title">
-                    <h1>KATEGORI</h1>
-                </div>
-
-                <ul class="cat-item">
-                    <li>
-                        <a href="#">
-                            <img alt="international-food-image" src="img/kebab.jpg">
-                            <div class="cat-name">
-                                <h1> Khas Luar Negeri </h1>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#"><img alt="beverage-image" src="img/minum.jpg">
-                            <div class="cat-name">
-                                <h1>Minuman</h1>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#"><img alt="cookies-image" src="img/cookies.jpg">
-                            <div class="cat-name">
-                                <h1>Kue</h1>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#"><img alt="meat-image" src="img/meat.jpg">
-                            <div class="cat-name">
-                                <h1>Olahan Daging</h1>
-                            </div>
-                        </a>
-                    </li>
-                </ul>
-                <ul class="cat-item">
-                    <li>
-                        <a href="#"><img alt="cuisine-image" src="img/cuisine.jpg">
-                            <div class="cat-name">
-                                <h1>Khas Daerah</h1>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#"><img alt="street-food-image" src="img/lumpia.jpg">
-                            <div class="cat-name">
-                                <h1>Jajanan Pasar</h1>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#"><img alt="soup-image" src="img/soup.jpg">
-                            <div class="cat-name">
-                                <h1>Soup</h1>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#"><img alt="vegetarian-image" src="img/veget.jpg">
-                            <div class="cat-name">
-                                <h1>Vegetarian</h1>
-                            </div>
-                        </a>
-                    </li>
-                </ul>
-                <br>
-                <br>
+            <div class="slide-nav">
+                <label for="img-4" class="prev">&#x2039;</label>
+                <label for="img-2" class="next">&#x203a;</label>
             </div>
-            <div class="sub animated rubberBand">
+
+        </li>
+
+        <input type="radio" name="rdo-btn" id="img-2">
+
+        <li class="slide-container">
+
+            <div class="slide">
+                <img alt="banner 2" src="img/img_nature_wide.jpg">
+            </div>
+
+            <div class="slide-nav">
+                <label for="img-1" class="prev">&#x2039;</label>
+                <label for="img-3" class="next">&#x203a;</label>
+            </div>
+
+        </li>
+
+        <input type="radio" name="rdo-btn" id="img-3">
+
+        <li class="slide-container">
+
+            <div class="slide">
+                <img alt="banner 3" src="img/img_snow_wide.jpg">
+            </div>
+
+            <div class="slide-nav">
+                <label for="img-2" class="prev">&#x2039;</label>
+                <label for="img-4" class="next">&#x203a;</label>
+            </div>
+
+        </li>
+
+        <input type="radio" name="rdo-btn" id="img-4">
+
+        <li class="slide-container">
+
+            <div class="slide">
+                <img alt="banner 4" src="img/img_nature_spring_forest.jpg">
+            </div>
+
+            <div class="slide-nav">
+                <label for="img-3" class="prev">&#x2039;</label>
+                <label for="img-1" class="next">&#x203a;</label>
+            </div>
+
+        </li>
+
+        <li class="dots">
+            <label for="img-1" id="img-dot-1"></label>
+            <label for="img-2" id="img-dot-2"></label>
+            <label for="img-3" id="img-dot-3"></label>
+            <label for="img-4" id="img-dot-4"></label>
+        </li>
+
+    </ul>
+
+    <div class="home-container">
+
+        <div class="info">
+            <div class="info-item animated fadeInLeft"><img src="img/home-info.png" alt="creative-cooking"></div>
+            <div class="info-item animated fadeInRight">
+                <h1>Berani Untuk Masak Sendiri Mulai Dari Sekarang</h1>
                 <br>
-                <br>
-                <br>
-                <h1>TETAP TERHUBUNG</h1>
-                <p>Isikan alamat email Anda pada kolom di bawah ini agar kami dapat memberi tahu Anda seputar informasi resep terbaru kami</p>
-                <br>
-                <div class="maile">
-                    <input name="subs" type="text" class="btn-mail" placeholder="Isikan alamat email Anda disini">
-                    <br>
-                    <br>
-                    <input type="submit" class="btn" value="Subscribe">
-                </div>
-                <br><br><br><br><br></div>
+                <ul>
+                    <li>Lebih Higienis dan Tak Perlu Diragukan</li>
+                    <li>Meningkatkan Keahlian Memasak</li>
+                    <li>Lebih Sehat</li>
+                    <li>Salurkan Kreativitas Ke Masakan</li>
+                </ul>
+            </div>
         </div>
 
+        <div class="category">
+
+            <div class="cat-title">
+                <h1>KATEGORI</h1>
+            </div>
+
+            <ul class="cat-item">
+                <li>
+                    <a href="#">
+                        <img alt="international-food-image" src="img/kebab.jpg">
+                        <div class="cat-name">
+                            <h1> Khas Luar Negeri </h1>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#"><img alt="beverage-image" src="img/minum.jpg">
+                        <div class="cat-name">
+                            <h1>Minuman</h1>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#"><img alt="cookies-image" src="img/cookies.jpg">
+                        <div class="cat-name">
+                            <h1>Kue</h1>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#"><img alt="meat-image" src="img/meat.jpg">
+                        <div class="cat-name">
+                            <h1>Olahan Daging</h1>
+                        </div>
+                    </a>
+                </li>
+            </ul>
+            <ul class="cat-item">
+                <li>
+                    <a href="#"><img alt="cuisine-image" src="img/cuisine.jpg">
+                        <div class="cat-name">
+                            <h1>Khas Daerah</h1>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#"><img alt="street-food-image" src="img/lumpia.jpg">
+                        <div class="cat-name">
+                            <h1>Jajanan Pasar</h1>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#"><img alt="soup-image" src="img/soup.jpg">
+                        <div class="cat-name">
+                            <h1>Soup</h1>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#"><img alt="vegetarian-image" src="img/veget.jpg">
+                        <div class="cat-name">
+                            <h1>Vegetarian</h1>
+                        </div>
+                    </a>
+                </li>
+            </ul>
+            <br>
+            <br>
+        </div>
+        <div class="sub animated rubberBand">
+            <br>
+            <br>
+            <br>
+            <h1>TETAP TERHUBUNG</h1>
+            <p>Isikan alamat email Anda pada kolom di bawah ini agar kami dapat memberi tahu Anda seputar informasi resep terbaru kami</p>
+            <br>
+            <div class="maile">
+                <input name="subs" type="text" class="btn-mail" placeholder="Isikan alamat email Anda disini">
+                <br>
+                <br>
+                <input type="submit" class="btn" value="Subscribe">
+            </div>
+            <br><br><br><br><br></div>
     </div>
+
+</div>
