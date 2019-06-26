c1745910e77efe9c5a50889df9563ea2d1eccfef
Muhammad Nur Alfian <muhammad.1220@students.amikom.ac.id>
2019-06-26 16:49:27 +0700

    ...

--- page/resep.php
+++ page/resep.php
@@ -1,11 +1,23 @@
 <div class="container">
 
-    <div class="home-container">
-        <div class="coba">
-            <h1>test</h1>
-        </div>
 
-    </div>
+    <h1>
+        Resep
+    </h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur laborum, harum, saepe at quia molestias, quaerat iusto fugit vitae rem a nemo sunt praesentium. Perferendis optio necessitatibus aliquid praesentium adipisci!</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam aliquid beatae atque ex at voluptatibus excepturi doloribus asperiores! Exercitationem excepturi animi iste officia iure accusamus sapiente numquam, saepe assumenda harum.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur laborum, harum, saepe at quia molestias, quaerat iusto fugit vitae rem a nemo sunt praesentium. Perferendis optio necessitatibus aliquid praesentium adipisci!</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam aliquid beatae atque ex at voluptatibus excepturi doloribus asperiores! Exercitationem excepturi animi iste officia iure accusamus sapiente numquam, saepe assumenda harum.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur laborum, harum, saepe at quia molestias, quaerat iusto fugit vitae rem a nemo sunt praesentium. Perferendis optio necessitatibus aliquid praesentium adipisci!</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam aliquid beatae atque ex at voluptatibus excepturi doloribus asperiores! Exercitationem excepturi animi iste officia iure accusamus sapiente numquam, saepe assumenda harum.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur laborum, harum, saepe at quia molestias, quaerat iusto fugit vitae rem a nemo sunt praesentium. Perferendis optio necessitatibus aliquid praesentium adipisci!</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam aliquid beatae atque ex at voluptatibus excepturi doloribus asperiores! Exercitationem excepturi animi iste officia iure accusamus sapiente numquam, saepe assumenda harum.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur laborum, harum, saepe at quia molestias, quaerat iusto fugit vitae rem a nemo sunt praesentium. Perferendis optio necessitatibus aliquid praesentium adipisci!</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam aliquid beatae atque ex at voluptatibus excepturi doloribus asperiores! Exercitationem excepturi animi iste officia iure accusamus sapiente numquam, saepe assumenda harum.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur laborum, harum, saepe at quia molestias, quaerat iusto fugit vitae rem a nemo sunt praesentium. Perferendis optio necessitatibus aliquid praesentium adipisci!</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam aliquid beatae atque ex at voluptatibus excepturi doloribus asperiores! Exercitationem excepturi animi iste officia iure accusamus sapiente numquam, saepe assumenda harum.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur laborum, harum, saepe at quia molestias, quaerat iusto fugit vitae rem a nemo sunt praesentium. Perferendis optio necessitatibus aliquid praesentium adipisci!</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam aliquid beatae atque ex at voluptatibus excepturi doloribus asperiores! Exercitationem excepturi animi iste officia iure accusamus sapiente numquam, saepe assumenda harum.</p>
 
 
 </div>
