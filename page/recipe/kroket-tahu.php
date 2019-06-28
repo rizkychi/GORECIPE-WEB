17184cf9851a43a16484e14d943c10a4e3f64bce
Awowawa <imadlz15@gmail.com>
2019-06-28 16:49:47 +0700

    tambah resep

---
+++ page/recipe/kroket-tahu.php
@@ -0,0 +1,33 @@
+<?php 
+    $title = 'Kroket Tahu';
+    $serve = 8;
+    $time = 40;
+    $star = 4.0;
+    $difficult = 'Cukup mudah';
+    $img = 'img/recipe/kroket-tahu.jpg';
+    $desc = 'Kroket Tahu, kreasi camilan praktis dan enak berbahan tahu nan lezat. Punya banyak stok tahu? Bikin Kroket Tahu saja yuk Bun! Kalau biasanya membuat kroket dengan kentang, kali ini Bunda bisa membuatnya menggunakan tahu. Lebih murah bahannya tapi rasa nggak kalah enak! Supaya rasanya makin mantap, Bunda bisa membuat isiannya dari daging ayam dan sayuran. Kroket Tahu bisa Bunda jadikan sebagai camilan atau bekal untuk anak yang pasti mengenyangkan dan bergizi. Apalagi membuatnya pakai Kobe Tepung Bakwan Kress, adonan kulit kroket jadi lebih gurih tanpa perlu banyak bumbu tambahan. Penasaran dengan rasanya? Langsung cobain yuk Bun!';
+    $youtube = 'RLE5uck_0Zo';
+    $ingredient = array(
+        '2 sendok makan minyak goreng',
+        '150 gram daging ayam giling',
+        '1/2 butir bawang bombay (cincang halus)',
+        '100 gram sayuran campur',
+        'garam secukupnya',
+        '1 butir telur',
+        '1 bungkus Kobe Tepung Bakwan Kress (75gr)',
+        '3 buah tahu putih ukuran sedang (kukus dan haluskan)',
+        'minyak goreng secukupnya (untuk menggoreng)'
+
+
+    );
+    $method = array(
+        'Panaskan minyak. Tumis daging ayam hingga berubah warna.',
+        'Masukkan bawang bombay dan sayuran campur. Aduk rata dan masak hingga matang.',
+        'Beri sedikit garam. Aduk rata, angkat dan dinginkan.',
+        'Campur semua bahan. Aduk hingga rata.',
+        'Ambil adonan kulit secukupnya. Isi dengan isian dan bentuk bulat lonjong.',
+        'Goreng dalam minyak panas hingga berwarna kuning kecoklatan. Tiriskan.',
+        'Kroket Tahu siap disajikan selagi hangat.'
+
+    );
+?>
