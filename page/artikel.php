af648d612b5134c7b45328f1ce673a25dad32606
anash98 <nash.ruddin522@gmail.com>
2019-06-29 22:55:26 +0700

    update content article page

--- page/artikel.php
+++ page/artikel.php
@@ -13,10 +13,10 @@
                         <img src="img/artikel/slider-1.jpg">
                     </div>
                     <div class="caption-konten">
-                        <h3>JUDUL</h3>
+                        <h3>6 Bahaya MSG Bagi Tubuh</h3>
                         <p>
-                            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin semper, ante vitae sollicitudin posuere, metus quam iaculis nibh, vitae scelerisque nunc massa eget pede. Sed velit urna, interdum vel, ultricies vel, faucibus at, quam.
-                            Donec elit est, consectetuer eget, consequat quis, tempus quis, wisi. Cras faucibus condimentum odio. Sed ac ligula. Aliquam at eros.
+                            Banyak orang yang suka menambahkan MSG atau bahasa populernya micin/vetsin ke dalam makanan, tapi tidak sedikit orang yang merasa dirugikan dengan menambahkan bahan masakan itu.
+                            Berikut ini kami akan memberikan segala macam ulasan mengenai MSG mulai dari bahanya untuk tubuh jika dikonsumsi berlebihan.
                             <a href="#">Baca Selengkapnya</a>
                         </p>
                     </div>
@@ -26,10 +26,10 @@
                         <img src="img/artikel/slider-2.jpg">
                     </div>
                     <div class="caption-konten">
-                        <h3>JUDUL</h3>
+                        <h3>Manfaat Konsumsi Bawang Putih</h3>
                         <p>
-                            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin semper, ante vitae sollicitudin posuere, metus quam iaculis nibh, vitae scelerisque nunc massa eget pede. Sed velit urna, interdum vel, ultricies vel, faucibus at, quam.
-                            Donec elit est, consectetuer eget, consequat quis, tempus quis, wisi. Cras faucibus condimentum odio. Sed ac ligula. Aliquam at eros.
+                            Bawang putih selain sebagai bumbu makanan juga banyak sekali khasiatnya terutama untuk kesehatan. Banyak penyakit yang bisa dicegah dan diobati dengan mengkonsumsi bawang putih.
+                            Dilansir Healthyfoodhouse.com, penelitian yang dilakukan kepada orang-orang yang mengkonsumsi bawang putih di pagi hari pada saat bangun tidur atau dalam keadaan perut kosong. Hasilnya mengejutkan karena bisa.
                             <a href="#">Baca Selengkapnya</a>
                         </p>
                     </div>
@@ -39,10 +39,9 @@
                         <img src="img/artikel/slider-3.jpg">
                     </div>
                     <div class="caption-konten">
-                        <h3>JUDUL</h3>
+                        <h3>Cara Menyimpan Roti Tawar Agar Tetap Fresh</h3>
                         <p>
-                            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin semper, ante vitae sollicitudin posuere, metus quam iaculis nibh, vitae scelerisque nunc massa eget pede. Sed velit urna, interdum vel, ultricies vel, faucibus at, quam.
-                            Donec elit est, consectetuer eget, consequat quis, tempus quis, wisi. Cras faucibus condimentum odio. Sed ac ligula. Aliquam at eros.
+                            Kebanyakan orang akan memilih sarapan dengan setangkup roti tawar yang diolesi selai untuk menu praktis pagi hari. Tapi tahukah anda jika anda salah dalam menyimpan roti tawar ini akan cepat menjamur dan tentu saja tidak layak lagi untuk di konsumsi. Berikut kami berikan cara menyimpan roti tawar agar awet dan fresh.
                             <a href="#">Baca Selengkapnya</a>
                         </p>
                     </div>
@@ -52,10 +51,10 @@
                         <img src="img/artikel/slider-4.jpg">
                     </div>
                     <div class="caption-konten">
-                        <h3>JUDUL</h3>
+                        <h3>Tips Belajar Memasak Untuk Pemula</h3>
                         <p>
-                            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin semper, ante vitae sollicitudin posuere, metus quam iaculis nibh, vitae scelerisque nunc massa eget pede. Sed velit urna, interdum vel, ultricies vel, faucibus at, quam.
-                            Donec elit est, consectetuer eget, consequat quis, tempus quis, wisi. Cras faucibus condimentum odio. Sed ac ligula. Aliquam at eros.
+                            Beberapa orang yang belum pernah memasak sering mengalami kesulitan diawal percobaan memasaknya dan mungkin saja pernah mengalami kecealakaan di dapur seperti kecipratan minyak panas atau teriris ketika memotong bawang dan sayuran. Kadang itu semua bisa menjadi salah satu alasan bagi anda yang baru belajar memasak malas untuk memasak lagi.
+                            Tapi jangan khawatir karena kami akan memberikan tips memasak untuk anda yang baru belajar memasak, mungkin setelah membacaca artikel di bawah ini anda akan tertarik untuk menjadi koki untuk orang-orang yang anda cintai.
                             <a href="#">Baca Selengkapnya</a>
                         </p>
                     </div>
@@ -74,14 +73,14 @@
                                 <img src="img/artikel/terbaru-1.jpg">
                             </span>
                             <div class="tanggal-artikel">
-                                <h3>1</h3>
-                                <p>Mei 2019</p>
+                                <h3>4</h3>
+                                <p>Juli 2019</p>
                             </div>
                         </div>
                         <div class="judul-rating">
-                            <h3>Garox Circulation</h3>
-                            <p>Oleh Mhamank Garox</p>
-                            <p style="float:right; margin:10px 20px 0px; 0px;"> 2.100 dilihat</p>
+                            <h3>Cara Menata Makanan Di Kulkas</h3>
+                            <p>Oleh GoRecipe</p>
+                            <p style="float:right; margin:10px 20px 0px; 0px;">256 dilihat</p>
                             <fieldset class="rating">
                                 <input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" title="Awesome - 5 stars"></label>
                                 <input type="radio" id="star4half" name="rating" value="4 and a half" /><label class="half" for="star4half" title="Pretty good - 4.5 stars"></label>
@@ -97,8 +96,9 @@
                         </div>
                         <div class="isi">
                             <p>
-                                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin semper, ante vitae sollicitudin posuere, metus quam iaculis nibh, vitae scelerisque nunc massa eget pede. Sed velit urna, interdum vel, ultricies vel, faucibus at, quam.
-                                Donec elit est, consectetuer eget, consequat quis, tempus quis, wisi. Cras faucibus condimentum odio. Sed ac ligula. Aliquam at eros. faucibus condimentum odio. Sed ac ligula. Aliquam at eros.
+                                Cara menyimpan makanan di kulkas memang susah-susah gampang, kalau tidak tahu cara yang tepat Anda akan susah mencari makanan yang ingin Anda ambil dan membuat kegunaan kulkas tidak efisien. Yang seharusnya memudahkan Anda agar makanan tidak cepat busuk, tapi makanan Anda lebih cepat busuk di dalam kulkas.
+                                Di mana yang salah ya? Mari kita baca tips di bawah ini: Telur
+                                Menyimpan telur di kulkas adalah hal yang mudah,
                             </p>
                             <div class="selengkapnya">
                                 <p>SELENGKAPNYA</p>
@@ -111,14 +111,14 @@
                                 <img src="img/artikel/terbaru-2.jpg">
                             </span>
                             <div class="tanggal-artikel">
-                                <h3>1</h3>
-                                <p>Mei 2019</p>
+                                <h3>2</h3>
+                                <p>Juli 2019</p>
                             </div>
                         </div>
                         <div class="judul-rating">
-                            <h3>Garox Circulation</h3>
-                            <p>Oleh Mhamank Garox</p>
-                            <p style="float:right; margin:10px 20px 0px; 0px;"> 2.100 dilihat</p>
+                            <h3>Tips Wajan Teflon Tetap Awet</h3>
+                            <p>Oleh GoRecipe</p>
+                            <p style="float:right; margin:10px 20px 0px; 0px;">312 dilihat</p>
                             <fieldset class="rating">
                                 <input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" title="Awesome - 5 stars"></label>
                                 <input type="radio" id="star4half" name="rating" value="4 and a half" /><label class="half" for="star4half" title="Pretty good - 4.5 stars"></label>
@@ -134,8 +134,8 @@
                         </div>
                         <div class="isi">
                             <p>
-                                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin semper, ante vitae sollicitudin posuere, metus quam iaculis nibh, vitae scelerisque nunc massa eget pede. Sed velit urna, interdum vel, ultricies vel, faucibus at, quam.
-                                Donec elit est, consectetuer eget, consequat quis, tempus quis, wisi. Cras faucibus condimentum odio. Sed ac ligula. Aliquam at eros. faucibus condimentum odio. Sed ac ligula. Aliquam at eros.
+                                Teflon atau wajan anti lengket adalah salah satu peralatan dapur yang wajib ada di rumah karena memberikan kemudahan dalam memasak. Setiap teflon selalu dilengkapi dengan lapisan anti lengket yang akan membuat makanan tidak lengket pada permukaan wajaan saat memasaknya.
+                                Namun sebagian orang pasti akan merasa kesulitan untuk merawat dan membersihkannya. Untuk itu kami akan memberikan tips merawat dan mencuci teflon
                             </p>
                             <div class="selengkapnya">
                                 <p>SELENGKAPNYA</p>
@@ -148,14 +148,14 @@
                                 <img src="img/artikel/terbaru-3.jpg">
                             </span>
                             <div class="tanggal-artikel">
-                                <h3>1</h3>
-                                <p>Mei 2019</p>
+                                <h3>30</h3>
+                                <p>Juni 2019</p>
                             </div>
                         </div>
                         <div class="judul-rating">
-                            <h3>Garox Circulation</h3>
-                            <p>Oleh Mhamank Garox</p>
-                            <p style="float:right; margin:10px 20px 0px; 0px;"> 2.100 dilihat</p>
+                            <h3>19 Superfood Untuk Anak</h3>
+                            <p>Oleh GoRecipe</p>
+                            <p style="float:right; margin:10px 20px 0px; 0px;">598 dilihat</p>
                             <fieldset class="rating">
                                 <input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" title="Awesome - 5 stars"></label>
                                 <input type="radio" id="star4half" name="rating" value="4 and a half" /><label class="half" for="star4half" title="Pretty good - 4.5 stars"></label>
@@ -171,8 +171,8 @@
                         </div>
                         <div class="isi">
                             <p>
-                                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin semper, ante vitae sollicitudin posuere, metus quam iaculis nibh, vitae scelerisque nunc massa eget pede. Sed velit urna, interdum vel, ultricies vel, faucibus at, quam.
-                                Donec elit est, consectetuer eget, consequat quis, tempus quis, wisi. Cras faucibus condimentum odio. Sed ac ligula. Aliquam at eros. faucibus condimentum odio. Sed ac ligula. Aliquam at eros.
+                                Dalam masa pertumbuan, seorang anak sangatlah membutuhkan gizi yang optimal seperti makanan kaya serat, mineral dan juga vitamin. Semakin bertambahnya usia anak maka bertambah pula aktivitasnya maka dari itu orang tuna wajib memberikan makanan yang bernutrisi yang cukup agar tumbuh kembangnya semakin maksimal.
+                                Setiap orang tua pastinya ingin memberikan makanan yang bergizi untuk anaknya. Jenis makanan super untuk anak
                             </p>
                             <div class="selengkapnya">
                                 <p>SELENGKAPNYA</p>
@@ -185,14 +185,14 @@
                                 <img src="img/artikel/terbaru-4.jpg">
                             </span>
                             <div class="tanggal-artikel">
-                                <h3>1</h3>
-                                <p>Mei 2019</p>
+                                <h3>27</h3>
+                                <p>Juni 2019</p>
                             </div>
                         </div>
                         <div class="judul-rating">
-                            <h3>Garox Circulation</h3>
-                            <p>Oleh Mhamank Garox</p>
-                            <p style="float:right; margin:10px 20px 0px; 0px;"> 2.100 dilihat</p>
+                            <h3>Menanak Nasi Agar Pulen</h3>
+                            <p>Oleh GoRecipe</p>
+                            <p style="float:right; margin:10px 20px 0px; 0px;">645 dilihat</p>
                             <fieldset class="rating">
                                 <input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" title="Awesome - 5 stars"></label>
                                 <input type="radio" id="star4half" name="rating" value="4 and a half" /><label class="half" for="star4half" title="Pretty good - 4.5 stars"></label>
@@ -208,8 +208,8 @@
                         </div>
                         <div class="isi">
                             <p>
-                                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin semper, ante vitae sollicitudin posuere, metus quam iaculis nibh, vitae scelerisque nunc massa eget pede. Sed velit urna, interdum vel, ultricies vel, faucibus at, quam.
-                                Donec elit est, consectetuer eget, consequat quis, tempus quis, wisi. Cras faucibus condimentum odio. Sed ac ligula. Aliquam at eros. faucibus condimentum odio. Sed ac ligula. Aliquam at eros.
+                                Makanan wajib orang Indonesia adalah nasi, jika makan tanpa nasi rasanya seperti tidak makan. Tetapi tidak semua orang bisa memasak nasi yang enak, meski terdengar sederhana nyatanya menanak nasi tidak semudah itu. Jika Anda biasanya memasak nasi namun terasa ber-air, lembek atau bahkan terlalu kering (pera). Tentu saja hal ini membuat nafsu makan Anda dan keluarga berkurang.
+                                Jika nasi yang dimasak terlalu lembek atau terlalu keras, maka kenikmatan makan
                             </p>
                             <div class="selengkapnya">
                                 <p>SELENGKAPNYA</p>
@@ -251,29 +251,29 @@
                         <div class="daftar-artikel">
                             <img src="img/artikel/slider-1.jpg">
                             <div class="judul-artikel">
-                                <h2>Lorem Ipsum aswasdw fwredd sadwdd</h2>
-                                <p>2.122 dilihat</p>
+                                <h2>6 Bahaya MSG Bagi Tubuh</h2>
+                                <p>10.199 dilihat</p>
                             </div>
                         </div>
                         <div class="daftar-artikel">
                             <img src="img/artikel/slider-2.jpg">
                             <div class="judul-artikel">
-                                <h2>Lorem Ipsum aswasdw fwredd sadwdd</h2>
-                                <p>2.122 dilihat</p>
+                                <h2>Manfaat Konsumsi Bawang Putih</h2>
+                                <p>9.678 dilihat</p>
                             </div>
                         </div>
                         <div class="daftar-artikel">
                             <img src="img/artikel/slider-3.jpg">
                             <div class="judul-artikel">
-                                <h2>Lorem Ipsum aswasdw fwredd sadwdd</h2>
-                                <p>2.122 dilihat</p>
+                                <h2>Cara Menyimpan Roti Tawar Agar Tetap Fresh</h2>
+                                <p>9.122 dilihat</p>
                             </div>
                         </div>
                         <div class="daftar-artikel">
                             <img src="img/artikel/slider-4.jpg">
                             <div class="judul-artikel">
-                                <h2>Lorem Ipsum aswasdw fwredd sadwdd</h2>
-                                <p>2.122 dilihat</p>
+                                <h2>Tips Belajar Memasak Untuk Pemula</h2>
+                                <p>8.506 dilihat</p>
                             </div>
                         </div>
                     </div>
